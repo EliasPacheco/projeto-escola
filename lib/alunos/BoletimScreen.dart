6c6825d2ebe144b7996a8579f048d40f58c318b3
Elias  Pacheco <63831841+EliasPacheco@users.noreply.github.com>
2024-02-28 19:06:03 -0300

    add colocar nota visão coordenação

--- lib/alunos/BoletimScreen.dart
+++ lib/alunos/BoletimScreen.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:escola/alunos/AlunoHome.dart';
-import 'package:flutter/material.dart';
 
 class BoletimScreen extends StatefulWidget {
   final String userType;
@@ -25,7 +25,6 @@ class _BoletimScreenState extends State<BoletimScreen> {
     super.initState();
     _aluno = widget.aluno;
 
-    // Modifique o código para buscar os dados corretamente no Firestore
     FirebaseFirestore.instance
         .collection('alunos')
         .doc(_aluno.serie)
@@ -35,8 +34,7 @@ class _BoletimScreenState extends State<BoletimScreen> {
         .then((DocumentSnapshot documentSnapshot) {
       if (documentSnapshot.exists) {
         setState(() {
-          _materias = documentSnapshot['materias'] ??
-              {}; // Usando ?? para tratar caso 'materias' seja nulo
+          _materias = documentSnapshot['materias'] ?? {};
         });
       } else {
         print('Documento do aluno não encontrado no Firestore.');
@@ -50,7 +48,7 @@ class _BoletimScreenState extends State<BoletimScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Boletim - ${_aluno.nome}'),
+        title: Text('Boletim - ' + _aluno.nome.toString().split(' ')[0]),
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -67,8 +65,8 @@ class _BoletimScreenState extends State<BoletimScreen> {
   List<Widget> _buildSubjectCards() {
     List<Widget> subjectCards = [];
 
-    if (_materias != null && _materias!.isNotEmpty) {
-      _materias!.forEach((subject, grades) {
+    if (_materias != null && _materias.isNotEmpty) {
+      _materias.forEach((subject, grades) {
         List<Widget> subjectRows = [];
 
         List<String> months = [
@@ -90,7 +88,11 @@ class _BoletimScreenState extends State<BoletimScreen> {
           String grade = grades[month] != null ? grades[month].toString() : '';
           Color color = grade.isNotEmpty ? Colors.blue : Colors.orange;
 
-          subjectRows.add(_buildSubjectRow(month, grade, color));
+          // Adiciona o ícone de lápis como ícone de edição
+          IconData editIcon = Icons.edit;
+
+          subjectRows
+              .add(_buildSubjectRow(subject, month, grade, color, editIcon));
         }
 
         Widget subjectCard =
@@ -98,9 +100,14 @@ class _BoletimScreenState extends State<BoletimScreen> {
         subjectCards.add(subjectCard);
       });
     } else {
-      // Adicione uma mensagem indicando que os dados estão sendo carregados
       subjectCards.add(
         Card(
+          elevation: 4,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          margin: EdgeInsets.only(bottom: 20),
+          color: Colors.grey[100],
           child: ListTile(
             title: Text('Carregando boletim...'),
           ),
@@ -119,6 +126,7 @@ class _BoletimScreenState extends State<BoletimScreen> {
         borderRadius: BorderRadius.circular(10),
       ),
       margin: EdgeInsets.only(bottom: 20),
+      color: Colors.grey[100],
       child: ExpansionTile(
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -127,7 +135,7 @@ class _BoletimScreenState extends State<BoletimScreen> {
               'Matéria: $subject',
               style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
-            Icon(iconData),
+            //Icon(iconData),
           ],
         ),
         children: <Widget>[
@@ -150,7 +158,8 @@ class _BoletimScreenState extends State<BoletimScreen> {
     );
   }
 
-  Widget _buildSubjectRow(String subject, String grade, Color color) {
+  Widget _buildSubjectRow(String subject, String month, String grade,
+      Color color, IconData editIcon) {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 8),
       child: Row(
@@ -158,19 +167,114 @@ class _BoletimScreenState extends State<BoletimScreen> {
         children: [
           Row(
             children: [
+              Icon(Icons.calendar_month, color: Colors.blue),
               SizedBox(width: 8),
               Text(
-                subject,
-                style: TextStyle(fontWeight: FontWeight.bold),
+                month,
+                style: TextStyle(
+                    fontFamily: 'RobotoMono',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16),
               ),
             ],
           ),
-          Text(
-            grade,
-            style: TextStyle(fontWeight: FontWeight.bold, color: color),
+          Row(
+            children: [
+              Text(
+                grade,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: color, fontSize: 20),
+              ),
+              if (widget.userType == 'Coordenacao')
+                IconButton(
+                  icon: Icon(
+                    editIcon,
+                    size: 20,
+                  ),
+                  onPressed: () {
+                    _showEditDialog(subject, month);
+                  },
+                )
+            ],
           ),
         ],
       ),
     );
+  }
+
+  void _showEditDialog(String subject, String month) {
+    TextEditingController _noteController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Editar Nota - $subject - $month'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Insira a nova nota:',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 12),
+              TextField(
+                controller: _noteController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Nota',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Fecha o diálogo
+              },
+              child: Text(
+                'Cancelar',
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                String newNote = _noteController.text;
+                _updateNoteInFirestore(subject, month, newNote);
+                Navigator.pop(context); // Fecha o diálogo
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+              ),
+              child: Text(
+                'Enviar',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _updateNoteInFirestore(String subject, String month, String newNote) {
+    FirebaseFirestore.instance
+        .collection('alunos')
+        .doc(_aluno.serie)
+        .collection('alunos')
+        .doc(_aluno.documentId)
+        .update({
+      'materias.$subject.$month': newNote,
+    }).then((value) {
+      print('Nota atualizada com sucesso!');
+      // Você pode atualizar localmente a estrutura de dados se necessário
+      setState(() {
+        _materias[subject][month] = newNote;
+      });
+    }).catchError((error) {
+      print('Erro ao atualizar nota: $error');
+    });
   }
 }
