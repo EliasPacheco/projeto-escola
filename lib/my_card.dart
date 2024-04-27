9774143bafbe8a9c1212c372835c7ccef2380fad
Elias  Pacheco <63831841+EliasPacheco@users.noreply.github.com>
2024-04-27 15:33:47 -0300

    alterado icone + animação homescreen

--- lib/my_card.dart
+++ lib/my_card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class MyCard extends StatelessWidget {
+class MyCard extends StatefulWidget {
   final String title;
   final IconData icon;
   final ImageProvider image;
@@ -15,61 +15,86 @@ class MyCard extends StatelessWidget {
     required this.icon,
     required this.borderColor,
     required this.onTap,
-    required this.image, required this.widthW, required this.heightH,
+    required this.image,
+    required this.widthW,
+    required this.heightH,
   });
+
+  @override
+  _MyCardState createState() => _MyCardState();
+}
+
+class _MyCardState extends State<MyCard> {
+  double _opacity = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    // Defina um pequeno atraso para iniciar a animação de fade
+    Future.delayed(Duration(milliseconds: 200), () {
+      setState(() {
+        _opacity = 1.0;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: onTap,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Color(0xff2E71E8),
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  color: Color(0xff2E71E8).withOpacity(0.5),
-                  spreadRadius: 1,
-                  blurRadius: 15,
-                  offset: Offset(0, 7), // changes position of shadow
-                ),
-              ],
-            ),
-            child: SizedBox(
-              width: 150,
-              height: 150,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    width: widthW,
-                    height: heightH,
-                    decoration:
-                        BoxDecoration(image: DecorationImage(image: image)),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    title,
-                    style: GoogleFonts.nunito(
-                      textStyle: const TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
+      onTap: widget.onTap,
+      child: AnimatedOpacity(
+        opacity: _opacity,
+        duration: Duration(milliseconds: 500), // Defina a duração da animação
+        curve: Curves.easeIn, // Adicione uma curva para a animação
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xff2E71E8),
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xff2E71E8).withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 15,
+                    offset: Offset(0, 7), // changes position of shadow
                   ),
                 ],
               ),
+              child: SizedBox(
+                width: 150,
+                height: 150,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: widget.widthW,
+                      height: widget.heightH,
+                      decoration:
+                          BoxDecoration(image: DecorationImage(image: widget.image)),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      widget.title,
+                      style: GoogleFonts.nunito(
+                        textStyle: const TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
